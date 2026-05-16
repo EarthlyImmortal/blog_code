8bfe32dbcda630459385b05fbae45e0075992684
EarthlyImmortal <3226779785@qq.com>
2026-05-16 23:04:37 +0800

    添加my_server和my_client

---
+++ metadata/my_client/client.cpp
@@ -0,0 +1,109 @@
+/*
+ *
+ * Copyright 2015 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <grpcpp/grpcpp.h>
+
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include "absl/flags/parse.h"
+#include "absl/log/initialize.h"
+#include "hello_world.grpc.pb.h"
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+using helloworld::Greeter;
+using helloworld::HelloReply;
+using helloworld::HelloRequest;
+
+class CustomHeaderClient
+{
+   public:
+    CustomHeaderClient(std::shared_ptr<Channel> channel)
+        : stub_(Greeter::NewStub(channel))
+    {
+    }
+
+    // 组装客户端的负载，发送并呈现来自服务器的响应
+    std::string SayHello(const std::string& user)
+    {
+        // 我们将发送给服务器的数据
+        HelloRequest request;
+        request.set_name(user);
+
+        // 存放我们从服务器期望收到的数据的容器
+        HelloReply reply;
+
+        // 客户端上下文。可用于向服务器传递额外信息
+        // 和/或调整某些 RPC 行为
+        ClientContext context;
+
+        // 设置要发送给服务器的自定义元数据
+        context.AddMetadata("custom-header", "Custom Value");
+
+        // 设置自定义二进制元数据
+        char bytes[8] = {'\0', '\1', '\2', '\3', '\4', '\5', '\6', '\7'};
+        context.AddMetadata("custom-bin", std::string(bytes, 8));
+
+        // 实际的 RPC 调用
+        Status status = stub_->SayHello(&context, request, &reply);
+
+        // 根据其状态进行处理
+        if (status.ok())
+        {
+            std::cout << "Client received initial metadata from server: "
+                      << context.GetServerInitialMetadata()
+                             .find("custom-server-metadata")
+                             ->second
+                      << std::endl;
+            std::cout << "Client received trailing metadata from server: "
+                      << context.GetServerTrailingMetadata()
+                             .find("custom-trailing-metadata")
+                             ->second
+                      << std::endl;
+
+            return reply.message();
+        }
+        else
+        {
+            std::cout << status.error_code() << ": " << status.error_message()
+                      << std::endl;
+            return "RPC failed";
+        }
+    }
+
+   private:
+    std::unique_ptr<Greeter::Stub> stub_;
+};
+
+int main(int argc, char** argv)
+{
+    absl::ParseCommandLine(argc, argv);
+    absl::InitializeLog();
+    // 实例化客户端。它需要一个通道，实际的 RPC 调用将基于该通道创建。
+    // 该通道模拟了到某个端点（本例中为 localhost:50051）的连接。
+    // 我们指明该通道未经过认证（使用了 InsecureChannelCredentials()）。
+    CustomHeaderClient greeter(grpc::CreateChannel(
+        "localhost:50051", grpc::InsecureChannelCredentials()));
+    std::string user("world");
+    std::string reply = greeter.SayHello(user);
+    std::cout << "Client received message: " << reply << std::endl;
+    return 0;
+}
---
+++ metadata/my_server/server.cpp
@@ -0,0 +1,107 @@
+/*
+ *
+ * Copyright 2015 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <grpcpp/grpcpp.h>
+
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include "absl/flags/parse.h"
+#include "absl/log/initialize.h"
+#include "hello_world.grpc.pb.h"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+using helloworld::Greeter;
+using helloworld::HelloReply;
+using helloworld::HelloRequest;
+
+// 服务器行为背后的逻辑和数据。
+class GreeterServiceImpl final : public Greeter::Service
+{
+    Status SayHello(ServerContext* context, const HelloRequest* request,
+                    HelloReply* reply) override
+    {
+        std::string prefix("Hello ");
+
+        // 获取客户端的初始元数据
+        std::cout << "Client metadata: " << std::endl;
+        const std::multimap<grpc::string_ref, grpc::string_ref> metadata =
+            context->client_metadata();
+        for (auto iter = metadata.begin(); iter != metadata.end(); ++iter)
+        {
+            std::cout << "Header key: " << iter->first << ", value: ";
+            // 检查是否为二进制值
+            size_t isbin = iter->first.find("-bin");
+            if ((isbin != std::string::npos) &&
+                (isbin + 4 == iter->first.size()))
+            {
+                std::cout << std::hex;
+                for (auto c : iter->second)
+                {
+                    std::cout << static_cast<unsigned int>(c);
+                }
+                std::cout << std::dec;
+            }
+            else
+            {
+                std::cout << iter->second;
+            }
+            std::cout << std::endl;
+        }
+
+        context->AddInitialMetadata("custom-server-metadata",
+                                    "initial metadata value");
+        context->AddTrailingMetadata("custom-trailing-metadata",
+                                     "trailing metadata value");
+        reply->set_message(prefix + request->name());
+        return Status::OK;
+    }
+};
+
+void RunServer()
+{
+    std::string server_address("0.0.0.0:50051");
+    GreeterServiceImpl service;
+
+    ServerBuilder builder;
+    // 在给定地址上监听，不使用任何认证机制。
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+    // 将“service”注册为用于与客户端通信的实例。
+    // 在这种情况下，它对应的是一个*同步*服务。
+    builder.RegisterService(&service);
+    // 最后组装服务器。
+    std::unique_ptr<Server> server(builder.BuildAndStart());
+    std::cout << "Server listening on " << server_address << std::endl;
+
+    // 等待服务器关闭。请注意，必须由其他某个线程负责关闭服务器，
+    // 此调用才会返回。
+    server->Wait();
+}
+
+int main(int argc, char** argv)
+{
+    absl::ParseCommandLine(argc, argv);
+    absl::InitializeLog();
+    RunServer();
+
+    return 0;
+}
